5eeed91888dca5ea1256b0d312c123e16bec10dc
heytomh <generict@gmail.com>
2018-10-14 01:00:40 -0700

    first file

---
+++ drawcircle.py
@@ -0,0 +1,18 @@
+import math
+import turtle
+
+def drawCircleTurtle(x, y, r):
+    turtle.up()
+    turtle.setpos(x+r, y)
+    turtle.down()
+
+    for i in range (0, 365, 1):
+        a = math.radians(i)
+        turtle.setpos(x + r*math.cos(a), y + r*math.sin(a))
+
+
+drawCircleTurtle(100,100,500)
+turtle.mainloop()
+
+# git test
+# git test
